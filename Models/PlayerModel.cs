d6fb6b2ad509439aa65cfe0a5a24f811d129d2c2
lfatima123 <66817058+lfatima123@users.noreply.github.com>
2020-06-12 18:24:13 -0400

    Changes to PlayerModel
    Add missing player attributes to the fetch so all data from the data provider is returned to the front-end

--- Models/PlayerModel.cs
+++ Models/PlayerModel.cs
@@ -2,17 +2,56 @@ using System.Runtime.Serialization;
 
 namespace sm_coding_challenge.Models
 {
+
     [DataContract]
     public class PlayerModel
     {
+
         [DataMember(Name = "player_id")]
         public string Id { get; set; }
+
+        [DataMember(Name = "entry_id")]
+        public string entryId { get; set; }
 
         [DataMember(Name = "name")]
         public string Name { get; set; }
 
         [DataMember(Name = "position")]
         public string Position { get; set; }
+
+        [DataMember(Name = "fld_goals_made")]
+        public int FldGoalsMade { get; set; }
+
+        [DataMember(Name = "fld_goals_att")]
+        public int FldGoalsAtt { get; set; }
+
+        [DataMember(Name = "extra_pt_made")]
+        public int ExtraPtMade { get; set; }
+
+        [DataMember(Name = "extra_pt_att")]
+        public int ExtraPtAtt { get; set; }
+
+        [DataMember(Name = "yds")]
+        public int Yds { get; set; }
+
+        [DataMember(Name = "tds")]
+        public int Tds { get; set; }
+
+        [DataMember(Name = "rec")]
+        public int Rec { get; set; }
+
+        [DataMember(Name = "att")]
+        public int Att { get; set; }
+
+        [DataMember(Name = "fum")]
+        public int Fum { get; set; }
+
+        [DataMember(Name = "cmp")]
+        public int cmp { get; set; }
+
+        [DataMember(Name = "int")]
+        public int intValue { get; set; }
+
     }
 }
 
